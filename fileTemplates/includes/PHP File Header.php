5bcb03f3adff9f41bf311a27a291747b1dcbe64a
Ionut Plesca <ionut.plesca@geopal-solutions.com>
2020-05-26 16:40:41 +0100

    PS-2019.3.4 <ionutplesca@Ionuts-iMac.local Create baseRefactoring.xml, markdown.xml, PHP Class.php, ide.general.xml, diff.xml, macOS copy.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, PHP Trait.php, customization.xml, Default.xml, PHP File Header.php, laf.xml, workspace.xml, vcs.xml, PHP Interface.php, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,5 @@
+/**
+ * @author    Ionut Plesca <ionut.plesca@geopal-solutions.com>
+ * @date      ${DATE} ${TIME}
+ * @copyright GeoPal Solutions
+ */
---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,12 @@
+<?php
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+#end
+
+#parse("PHP File Header.php")
+#parse("PHP Class Doc Comment.php")
+class ${NAME}
+{
+
+}
